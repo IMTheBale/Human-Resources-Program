bdb15f50687fc898ea696bffd2fde3847f797774
IMTheBale <johnny.bale.play@gmail.com>
2023-10-09 10:53:31 +0300

    first commit

---
+++ script.py
@@ -0,0 +1,8 @@
+assessment_score = 85
+location = "Canada"
+if (location == "US" or location == "Canada") and assessment_score >= 75:
+ print(f"Your assessment score is {assessment_score}/100! You're hired!")
+elif location != "US" and location != "Canada" and assessment_score >= 75:
+ print(f"Apologies, you are based in {location} but this role requires you to be based in the US/Canada. Would you be interested in another role?")
+else:
+ print(f"Thank you for your interest, we regret to inform you that we will not be moving forward with your application.")
